2d66a38f88b9ad2d8459f1452846b679edb02c1e
서유현_T5107 <uhs1038@gmail.com>
2023-05-08 06:57:36 +0000

    feature) 주석 추가

--- code/custom/custom_model.py
+++ code/custom/custom_model.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForSequenceClassification, PreTrainedModel
 
-
 class CustomModel(PreTrainedModel):
+    """새로운 레이어를 추가하거나, loss fucntion을 수정하는 등, 모델을 커스텀 하기 위한 클래스입니다."""    
     def __init__(self, config):
         super().__init__(config)
         self.bert = AutoModelForSequenceClassification.from_pretrained(
