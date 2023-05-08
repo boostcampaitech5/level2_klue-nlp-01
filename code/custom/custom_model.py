f64a1b31ffbad911d8e5bb6e892b4cc071d5ee54
서유현_T5107 <uhs1038@gmail.com>
2023-05-08 07:08:05 +0000

    feature) 쓰지 않는 모듈 정리 및 커스텀 모델 주석 추가

--- code/custom/custom_model.py
+++ code/custom/custom_model.py
@@ -1,7 +1,10 @@
 from transformers import AutoModelForSequenceClassification, PreTrainedModel
 
 class CustomModel(PreTrainedModel):
-    """새로운 레이어를 추가하거나, loss fucntion을 수정하는 등, 모델을 커스텀 하기 위한 클래스입니다."""    
+    """새로운 레이어를 추가하거나, loss fucntion을 수정하는 등, 모델을 커스텀 하기 위한 클래스입니다.
+    
+       pretrain된 모델을 불러올때, 모델마다 무엇을 input해야 하는지 다를 수 있기 때문에, 주의 하셔야 합니다.
+    """    
     def __init__(self, config):
         super().__init__(config)
         self.bert = AutoModelForSequenceClassification.from_pretrained(
--- code/train.py
+++ code/train.py
@@ -3,7 +3,7 @@ import sklearn
 import numpy as np
 
 from sklearn.metrics import accuracy_score
-from transformers import AutoConfig, AutoModelForSequenceClassification, TrainingArguments, EarlyStoppingCallback
+from transformers import AutoConfig, TrainingArguments, EarlyStoppingCallback
 from load_data import *
 from custom.custom_trainer import CustomTrainer
 from custom.custom_model import CustomModel
