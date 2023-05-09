8bcd6a5cadf6a83b5ae33a8f9f30d701e093aa0b
jiwonii97 <wldnjs3633@naver.com>
2023-05-09 16:37:15 +0000

    feature) entity tokenizing을 적용한 train 코드 추가

--- code/train.py
+++ code/train.py
@@ -112,3 +112,65 @@ def train(config, device):
     # train model
     trainer.train()
     model.save_pretrained(CONFIG.OUTPUT_PATH)
+
+def custom_train(config, device):
+    train_config = config.train
+
+    # model_name 및 tokenizer 호출
+    model_name = config.model_name
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    
+    # entity special token를 tokenizer에 추가
+    special_token_list = []
+    with open("custom/entity_special_token.txt", "r", encoding="UTF-8") as f:
+        for token in f:
+            special_token_list.append(token.split("\n")[0])
+
+    tokenizer.add_special_tokens(
+        {"additional_special_tokens": list(set(special_token_list))}
+    )
+
+    # make dataset for pytorch.
+    train_dataset, val_dataset = load_train_dataset(config['path'], tokenizer, config.tokenizer)
+
+    #setting model hyperparameter
+    model_config = AutoConfig.from_pretrained(model_name)
+    model_config.num_labels = CONFIG.NUM_LABELS
+
+
+    # model = CustomModel(config=model_config)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, config=model_config)
+    model.resize_token_embeddings(len(tokenizer))
+    model.to(device)
+
+    training_args = TrainingArguments(
+        report_to=CONFIG.WANDB,
+        seed=config.seed,
+        output_dir=train_config.output_dir,
+        save_total_limit=train_config.save_total_limit,
+        save_strategy=train_config.save_strategy,
+        per_device_train_batch_size=config.batch_size,
+        per_device_eval_batch_size=config.batch_size,
+        logging_dir=train_config.logging_dir,
+        logging_steps=train_config.logging_steps,
+        evaluation_strategy=train_config.evaluation_strategy,
+        load_best_model_at_end=train_config.load_best_model_at_end,
+        num_train_epochs=train_config.num_train_epochs,
+        learning_rate=train_config.learning_rate,
+        warmup_steps=train_config.warmup_steps,
+        weight_decay=train_config.weight_decay
+    )
+
+    trainer = CustomTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(
+            early_stopping_patience=train_config.early_stopping_patience)]
+    )
+
+    # train model
+    trainer.train()
+    model.save_pretrained(CONFIG.OUTPUT_PATH)
