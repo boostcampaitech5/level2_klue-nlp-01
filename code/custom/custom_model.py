93e4a0900750f46cefaa507946a97f1b6539743f
서유현_T5107 <uhs1038@gmail.com>
2023-05-10 04:41:39 +0000

    feature) 하드코딩 수정

--- code/custom/custom_model.py
+++ code/custom/custom_model.py
@@ -5,10 +5,10 @@ class CustomModel(PreTrainedModel):
     
        pretrain된 모델을 불러올때, 모델마다 무엇을 input해야 하는지 다를 수 있기 때문에, 주의 하셔야 합니다.
     """    
-    def __init__(self, config):
-        super().__init__(config)
+    def __init__(self, model_config, config):
+        super().__init__(model_config)
         self.bert = AutoModelForSequenceClassification.from_pretrained(
-            "bert-base-multilingual-cased", num_labels=30)
+            config.model_name, num_labels=30)
 
     def forward(self, input_ids, attention_mask, token_type_ids, labels):
         x = self.bert(input_ids=input_ids, attention_mask=attention_mask,
