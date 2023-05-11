4c92254687d9ece3aad4874f51b69e48eba24001
jiwonii97 <wldnjs3633@naver.com>
2023-05-11 03:25:03 +0000

    feature) custom 모델 inference를 위한 함수 추가

--- code/inference.py
+++ code/inference.py
@@ -40,7 +40,6 @@ def test(model, tokenized_sent, device, config):
 
     return np.concatenate(output_pred).tolist(), np.concatenate(output_prob, axis=0).tolist()
 
-
 def base_inference(config, device):
     """
       주어진 dataset csv 파일과 같은 형태일 경우 inference 가능한 코드입니다.
@@ -91,5 +90,61 @@ def base_inference(config, device):
     output.to_csv(file_path, index=False)
     #### 필수!! ##############################################
     print('---- Finish! ----')
+
+def custom_inference(config, device):
+    """
+      주어진 dataset csv 파일과 같은 형태일 경우 inference 가능한 코드입니다.
+    """
+    inference_config = config.inference
+    
+    # 토크나이저 호출
+    tokenizer_name = config.model_name
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    
+    # entity special token를 tokenizer에 추가
+    special_token_list = []
+    with open("custom/entity_special_token.txt", "r", encoding="UTF-8") as f:
+        for token in f:
+            special_token_list.append(token.split("\n")[0])
+
+    tokenizer.add_special_tokens(
+        {"additional_special_tokens": list(set(special_token_list))}
+    )
+
+    if config.last_file:
+        # 저장된 모델 호출
+        last_log = sorted(os.listdir(CONFIG.LOGDIR_PATH))[-1]
+        inference_dir = last_log
+    else:
+        inference_dir = config.inference_file
+    
+    model_dir = f"./logs/{inference_dir}/best_model"
+    model = AutoModelForSequenceClassification.from_pretrained(model_dir)
+    model.resize_token_embeddings(len(tokenizer))
+    model.to(device)
+
+    # test 데이터셋 호출
+    test_dataset_dir = config.path.test_path
+    test_id, test_dataset, test_label = load_test_dataset(test_dataset_dir, tokenizer, config.tokenizer)
+    Re_test_dataset = RE_Dataset(test_dataset, test_label)
+
+    # 정답 예측
+    pred_answer, output_prob = test(model, Re_test_dataset, device, config)  # model에서 class 추론
+    pred_answer = num_to_label(pred_answer)  # 숫자로 된 class를 원래 문자열 라벨로 변환.
+
+    # 예측된 정답을 DataFrame으로 저장
+    #########################################################
+    # 아래 directory와 columns의 형태는 지켜주시기 바랍니다.
+    output = pd.DataFrame({'id': test_id, 'pred_label': pred_answer, 'probs': output_prob, })
+
+    if not os.path.exists(CONFIG.PREDICTTION_PATH):
+        os.makedirs(CONFIG.PREDICTTION_PATH)
+        
+    if not os.path.exists(os.path.join(inference_config.output_dir, inference_dir)):
+        os.makedirs(os.path.join(inference_config.output_dir, inference_dir))
+        
+    # 최종적으로 완성된 예측한 라벨 csv 파일 형태로 저장.
+    file_path = os.path.join(inference_config.output_dir, inference_dir, inference_config.output_file)
+    output.to_csv(file_path, index=False)
     #### 필수!! ##############################################
     print('---- Finish! ----')
