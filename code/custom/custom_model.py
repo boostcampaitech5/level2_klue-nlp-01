34b9daa5d698619fe96deebe6c9ab1bd336208ef
서유현_T5107 <uhs1038@gmail.com>
2023-05-11 02:07:59 +0000

    feature) custom model에 스페셜 토큰 추가

--- code/custom/custom_model.py
+++ code/custom/custom_model.py
@@ -5,10 +5,14 @@ class CustomModel(PreTrainedModel):
     
        pretrain된 모델을 불러올때, 모델마다 무엇을 input해야 하는지 다를 수 있기 때문에, 주의 하셔야 합니다.
     """    
-    def __init__(self, model_config, model_name):
+
+    def __init__(self, model_name, model_config, tokenizer):
         super().__init__(config=model_config)
         self.bert = AutoModelForSequenceClassification.from_pretrained(
             model_name, num_labels=30)
+        self.tokenizer = tokenizer
+        
+        self.bert.resize_token_embeddings(len(tokenizer))
 
     def forward(self, input_ids, attention_mask, token_type_ids, labels):
         x = self.bert(input_ids=input_ids, attention_mask=attention_mask,
