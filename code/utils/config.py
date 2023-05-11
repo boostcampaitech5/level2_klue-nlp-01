5732d67f13f2f511a8b93ec5f48a00fe96104e7b
jiwonii97 <wldnjs3633@naver.com>
2023-05-11 03:21:09 +0000

    feature) args에 추가로 받은 변수 내용 추가

--- code/utils/config.py
+++ code/utils/config.py
@@ -4,6 +4,13 @@ from constants import CONFIG
 def load_config(args):
     """config.yaml과 args를 불러서 config로 반환합니다."""
     config = OmegaConf.load(CONFIG.CONFIG_PATH)
-    config.do_inference = args.inference
+    
+    if args.inference or args.last_file:
+        config.only_inference = True
+    else:
+        config.only_inference = False
+    config.inference_dir = args.inference
+    config.last_file = args.last_file
+    config.inference_file = args.inference
 
     return config
