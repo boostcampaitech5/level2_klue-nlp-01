714bf1ff643c8f66c461d886879171dc69afd8a7
서유현_T5107 <uhs1038@gmail.com>
2023-05-12 02:35:13 +0000

    feature) wandb 프로젝트명 변경

--- code/run.py
+++ code/run.py
@@ -29,7 +29,7 @@ def main(args):
     else:
         if args.custom:
             ## wandb 설정
-            wandb.init(project="KLUE-RE", name = folder_name)
+            wandb.init(project="KLUE-RE_device_into_focal_loss", name = folder_name)
             custom_train(config, device)
         else:
             base_train(config, device)
