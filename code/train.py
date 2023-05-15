ac64647a89e2ee39d8af0792299f17d3a27ecf03
jiwonii97 <wldnjs3633@naver.com>
2023-05-15 17:35:56 +0000

    fix) num_labels 데이터 적용

--- code/train.py
+++ code/train.py
@@ -27,18 +27,19 @@ def klue_re_micro_f1(preds, labels):
                   'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
                   'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
                   'per:religion']
-    no_relation_label_idx = label_list.index("no_relation")
-    label_indices = list(range(len(label_list)))
-    label_indices.remove(no_relation_label_idx)
+    # no_relation_label_idx = label_list.index("no_relation")
+    # label_indices = list(range(len(label_list)))
+    label_indices = list(range(NUM_LABELS))
+    # label_indices.remove(no_relation_label_idx)
     return sklearn.metrics.f1_score(labels, preds, average="micro", labels=label_indices) * 100.0
 
 
 def klue_re_auprc(probs, labels):
     """KLUE-RE AUPRC (with no_relation)"""
-    labels = np.eye(30)[labels]
+    labels = np.eye(NUM_LABELS)[labels]
 
-    score = np.zeros((30,))
-    for c in range(30):
+    score = np.zeros((NUM_LABELS,))
+    for c in range(NUM_LABELS):
         targets_c = labels.take([c], axis=1).ravel()
         preds_c = probs.take([c], axis=1).ravel()
         precision, recall, _ = sklearn.metrics.precision_recall_curve(
