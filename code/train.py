2f6eeae7e857f257d00970a7f4fdf338a20020df
서유현_T5107 <uhs1038@gmail.com>
2023-05-15 07:01:36 +0000

    feature) dropout 변경

--- code/train.py
+++ code/train.py
@@ -100,7 +100,7 @@ def base_train(config, device):
     # setting model hyperparameter
     model_config = AutoConfig.from_pretrained(model_name)
     model_config.num_labels = CONFIG.NUM_LABELS
-    model_config.hidden_dropout_prob = 0.3
+    model_config.hidden_dropout_prob = 0.1
 
     # model = CustomModel(config=model_config)
     model = AutoModelForSequenceClassification.from_pretrained(model_name, config=model_config)
