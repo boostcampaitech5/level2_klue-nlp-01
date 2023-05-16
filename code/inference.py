b20616d12e502b26b7826c76ad9173d2c7f0e7b1
서유현_T5107 <uhs1038@gmail.com>
2023-05-16 19:39:05 +0000

    feature) 데이터셋 호출 변경

--- code/inference.py
+++ code/inference.py
@@ -273,10 +273,10 @@ def custom_val_inference(config, device):
     ## 변경필요
     # val 데이터셋 호출
     val_dataset_dir = os.path.join(f"./logs/{inference_dir}/", config.path.split_nopreprocess_val_path)
-    val_dataset = custom.load_data(val_dataset_dir)
+    val_dataset = load_data(val_dataset_dir)
     val_id = val_dataset['id']
-    val_label = custom.label_to_num(val_dataset['label'].values)
-    tokenized_val = custom.tokenized_dataset(val_dataset, tokenizer, config.tokenizer)
+    val_label = label_to_num(val_dataset['label'].values)
+    tokenized_val = tokenized_dataset(val_dataset, tokenizer, config.tokenizer)
     Re_val_dataset = RE_Dataset(tokenized_val, val_label)
 
     # 정답 예측
